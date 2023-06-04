66887a042354051e6be7d928149462754ac5ab12
miloa2002 <agudelo.tapias@gmail.com>
2023-06-04 13:36:21 -0500

    bebidasapi con reactjs

---
+++ src/components/ModalBebida.jsx
@@ -0,0 +1,41 @@
+import { Modal, Image } from "react-bootstrap"
+import useBebidas from "../hooks/UseBebidas"
+
+const ModalBebida = () => {
+    const { modal, handleModalClick, receta } = useBebidas()
+
+    const mostrarIngredientes = () =>{
+        let ingredientes = []
+
+        for(let i = 1; i< 16; i++){
+            if (receta[`strIngredient${i}`]){
+                ingredientes.push(
+                    <li>{receta[`strIngredient${i}`]} {receta[`strMeasure${i}`]}</li>
+                )
+            }
+        }
+        return ingredientes
+    }
+
+  return (
+      <Modal show={modal} onHide={handleModalClick}>
+        <Image 
+              src={receta.strDrinkThumb}
+              alt={`imagen receta ${receta.strDrink}`}
+        />
+        <Modal.Header>
+              <Modal.Title>{receta.strDrink}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <div className="p-3">
+                <h2>Instrucciones</h2>
+                  {receta.strInstructions}
+                <h2>Ingredientes cantidades</h2>
+                  {mostrarIngredientes()}
+            </div>
+        </Modal.Body>
+    </Modal>
+  )
+}
+
+export default ModalBebida
